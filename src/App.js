5fab386706daddc4f523740ccae7d1d6c2733930
upendra <upendrapanwar456@gmail.com>
2024-01-17 18:44:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          TUTORIAL FOR GOOGLE ANALYTICS..............
+        😄TUTORIAL FOR GOOGLE ANALYTICS..............
         </h1>
         <h1>LIVE REACT APPLICATION</h1>
       </header>
