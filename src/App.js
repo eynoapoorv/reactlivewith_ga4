aee84d2d59c5de137e5da0dd19555ed3260346d5
upendra <upendrapanwar456@gmail.com>
2024-01-17 18:46:49 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-        😄TUTORIAL FOR GOOGLE ANALYTICS..............
+        <h1 className="App-logo">😄</h1>
+        <h1 >
+        TUTORIAL FOR GOOGLE ANALYTICS..............
         </h1>
         <h1>LIVE REACT APPLICATION</h1>
       </header>
